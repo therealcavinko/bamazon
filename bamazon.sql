84d1d6e5dde927a9efbb94dea8a205974cb51c0a
Calvin Cao <calvinknockout@gmail.com>
2020-01-26 23:41:43 -0500

    sql done

--- bamazon.sql
+++ bamazon.sql
@@ -16,12 +16,12 @@ CREATE TABLE products(
 Select * FROM products;
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity) 
-VALUES (100, "yeezys", "shoes", 79.99, 20),
-	   (200, "Air Force 1s", "shoes", 99.99, 10),
-	   (300, "Louis Vuitton Belt", "belts", 29.99, 5),
-	   (400, "fanny pack", "bags", 129.99, 14),
-	   (500, "purse", "bags", 39.99, 15),
-	   (600, "supreme t-shirt", "shirts", 19.99, 19),
-	   (700, "gucci shirt", "shirts", 49.99, 11),
-	   (800, "jeans", "pants", 69.99, 10),
-	   (900, "sweatpants", "pants", 9.99, 19)
+VALUES (100, "yeezys", "shoes", 700.99, 20),
+	   (200, "Air Force 1s", "shoes", 65.99, 10),
+	   (300, "Louis Vuitton Belt", "belts", 650.99, 5),
+	   (400, "fanny pack", "bags", 50.99, 10),
+	   (500, "purse", "bags", 890.99, 15),
+	   (600, "supreme t-shirt", "shirts", 1765.99, 19),
+	   (700, "gucci shirt", "shirts", 467.99, 17),
+	   (800, "jeans", "pants", 60.99, 10),
+	   (900, "sweatpants", "pants", 9.99, 12)
